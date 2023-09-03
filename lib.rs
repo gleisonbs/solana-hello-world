d320d5b51e20081ad9a69bf330c778928ffa5173
Gleison Batista <gleisonbs@gmail.com>
2023-09-03 20:42:20 -0300

    feat: add say_hello counter

--- lib.rs
+++ lib.rs
@@ -9,10 +9,36 @@ mod hello_world {
     use super::*;
 
     pub fn say_hello(_ctx: Context<SayHello>) -> Result<()> {
+        let counter = &mut _ctx.accounts.counter;
+        counter.count += 1;
         msg!("Hello World");
+        Ok(())
+    }
+
+    pub fn initialize_count(_ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut _ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Initialized new count. Current value: {}!", counter.count);
         Ok(())
     }
 }
 
+#[account]
+pub struct Counter {
+    count: u64,
+}
+
 #[derive(Accounts)]
-pub struct SayHello {}
+pub struct SayHello<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
